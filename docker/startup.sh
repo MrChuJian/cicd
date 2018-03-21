f989743d2535f79f02ab5e35f39741d7acaefb7f
zhengzhiwen <863293236@qq.com>
2018-03-21 21:02:03 +0800

    33

--- docker/startup.sh
+++ docker/startup.sh
@@ -5,6 +5,5 @@ if [ -z $log_level ]; then
   export log_level=WARN
 fi
 
-JAVA_OPTS="$JAVA_OPTS -Duser.timezone=GMT+08 -Djava.security.egd=file:/dev/./urandom"
+java -jar app.jar
 
-java $@ $JAVA_OPTS -jar app.jar
